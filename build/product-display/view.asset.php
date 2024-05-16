c10761af5fa7a5e264bf5fc60c74bb7a536f719b
Taras <office@yourfuture.com.ua>
2024-05-16 17:41:00 +0300

    rebuild

--- build/product-display/view.asset.php
+++ build/product-display/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-element', 'wp-url'), 'version' => 'e1d5b36715dd7857cbea');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-element', 'wp-url'), 'version' => '560da57f543335d68b66');
